a39eedc5ec7dbda4cf09e12271cb87d8d086051d
Zeomite <105901968+Zeomite@users.noreply.github.com>
2024-09-20 20:20:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { resolve } = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Stock portfolio analysis API!');
+});
+
+app.get('/calculate-returns', (req, res) => {
+  const boughtAt = parseFloat(req.query.boughtAt);
+  const marketPrice = parseFloat(req.query.marketPrice);
+  const quantity = parseFloat(req.query.quantity);
+  const returns = (marketPrice - boughtAt) * quantity;
+  res.send(returns.toString());
+});
+
+app.get('/total-returns', (req, res) => {
+  const stock1 = parseFloat(req.query.stock1);
+  const stock2 = parseFloat(req.query.stock2);
+  const stock3 = parseFloat(req.query.stock3);
+  const stock4 = parseFloat(req.query.stock4);
+  const totalReturns = stock1 + stock2 + stock3 + stock4;
+  res.send(totalReturns.toString());
+});
+
+app.get('/calculate-return-percentage', (req, res) => {
+  const boughtAt = parseFloat(req.query.boughtAt);
+  const returns = parseFloat(req.query.returns);
+  const returnPercentage = (returns / boughtAt) * 100;
+  res.send(returnPercentage.toString());
+});
+
+app.get('/total-return-percentage', (req, res) => {
+  const stock1 = parseFloat(req.query.stock1);
+  const stock2 = parseFloat(req.query.stock2);
+  const stock3 = parseFloat(req.query.stock3);
+  const stock4 = parseFloat(req.query.stock4);
+  const totalReturnPercentage = stock1 + stock2 + stock3 + stock4;
+  res.send(totalReturnPercentage.toString());
+});
+
+app.get('/status', (req, res) => {
+  const returnPercentage = parseFloat(req.query.returnPercentage);
+  const status = returnPercentage > 0 ? 'profit' : 'loss';
+  res.send(status);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
